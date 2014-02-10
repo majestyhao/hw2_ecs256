7014ec9b5b587265b176180a8ff51dd4bdfcfd6d
Hao Fu <jamesfuhao@gmail.com>
2014-02-09 19:57:09 -0800

    solve for pi

--- test.R
+++ test.R
@@ -1,33 +1,12 @@
-# simulate the event fliping coin for k times, p is the prob of head
-bf<-function(k,p){
-  nhead = 0
-  bhead=0
-  nonbhead=0
-  for (i in 1:k){
-    flip = sample(0:1,1,prob=c((1-p),p))
-    nhead = nhead+flip
-    nonbhead = nonbhead+flip
-    if (flip == 1){
-      bonus = sample(0:1,1,prob=c((1-p),p))
-      nhead = nhead+bonus
-      bhead = bhead+bonus
-    }  
-  }
-  c(nhead,nonbhead,bhead)
+P <- matrix(c(1/2, 1/2, 0, 1/3, 1/3, 1/3, 0, 1/2, 1/2), nrow = 3, ncol = 3, byrow = TRUE)
+
+findpi1 <- function(p) {
+  n <- nrow(p)
+  imp <- diag(n) - t(p) # I - P', left side
+  rhs <- c(rep(0, n - 1), 1) # form right-hand-side vector
+  pivec <- solve(imp, rhs)
+  return(pivec)
 }
-#repeat the event for nreps times to calculate corr
-bfsim<-function(k,p,nreps){
-  nheadvec <- c()
-  nonbheadvec <- c()
-  for (i in 1:nreps){
-    temp <- bf(k,p)
-    nheadvec[i] <- temp[1]
-    nonbheadvec[i] <- temp[2]
-  }
-  print(nheadvec)
-  # cat("cov:")
-  # print(cov(nheadvec,nonbheadvec))
-  p <- cov(nheadvec,nonbheadvec)/((var(nheadvec))^(1/2)+(var(nonbheadvec))^(1/2)) 
-  p
-}
-bfsim(10,0.5,10000)
+
+pi1 <- findpi1(P)
+pi1
